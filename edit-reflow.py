98e555e971e860a5aee6f022db94b4fa29cdf735
Silas S. Brown <ssb22@cam.ac.uk>
2021-04-05 10:34:36 +0100

    Update edit-reflow.py

--- edit-reflow.py
+++ edit-reflow.py
@@ -2,19 +2,21 @@
 # (should work in either Python 2 or Python 3)
 
 # Edit enlarged documents made by reflow.c
-# (c) Silas S. Brown, 2006-2009, 2012, 2020.  Version 1.3
+# (c) Silas S. Brown, 2006-2009, 2012, 2020-2021.  Version 1.31
 # 
 # Shows documentation on startup (or you can read it below)
 
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # Where to find history:
 # on GitHub at https://github.com/ssb22/scan-reflow
