7b8f8ee786a29a4ff8a4307dd0a9e4651f2b3fe2
Silas S. Brown <ssb22@cam.ac.uk>
2026-01-11 08:28:30 +0000

    work around Nook Simple Touch heading hyphenation bug

--- epub-backport.sh
+++ epub-backport.sh
@@ -17,8 +17,11 @@
 if ! [ "$1" ]; then echo "Syntax: $0 epub-files"
                     echo "(backports them inline)"
                     exit 1; fi
+set -e
 T="$(mktemp XXXXXX.epub)"
 for N in $@; do
     if ! ebook-convert "$N" "$T" --output-profile nook --epub-version 2 --linearize --no-default-epub-cover; then rm "$T"; exit 1; fi
+    # Work around bug in old Nook Simple Touch: when an inserted &shy; becomes visible on the last word of an otherwise 1-line "display: block" element (usually a heading in big print) the rest of the word disappears.  Workaround is to set the heading to display inline: it's still separated from the surrounding paragraphs that are display block (unless two headings together) but the old software is kludged into rendering it more correctly:
+    mkdir "$T-unpacked" && cd "$T-unpacked" && unzip "../$T" stylesheet.css && $( (which python || which python3 || which python2.7)2>/dev/null) -c 'import re;o="{".join((i.replace("display: block","display: inline") if re.search("font-size: 1.[1-9]",i) else i) for i in open("stylesheet.css").read().split("{"));open("stylesheet.css","w").write(o)' && zip -9X ../"$T" stylesheet.css && cd .. && rm -rf "$T-unpacked"
     mv "$T" "$N"
 done
