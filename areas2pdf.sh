64ed7cade5ffd48fe356a0bc1a1acaa757c8194b
Silas S. Brown <ssb22@cam.ac.uk>
2026-02-17 08:10:29 +0000

    fix areas2pdf portrait orientation on modern bash

--- areas2pdf.sh
+++ areas2pdf.sh
@@ -2,7 +2,7 @@
 
 # Gather saved areas from GIMP temp directory, put them in
 # order, and convert to PDF using pdflatex
-# (c) Silas S. Brown 2005,2007,2010,2012,2018-2019,2021-22 (version 1.11).
+# (c) Silas S. Brown 2005,2007,2010,2012,2018-2019,2021-22,2026 (version 1.12).
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -39,8 +39,9 @@ TmpDir=$(mktemp -d /tmp/areas2pdfXXXXXX)
 cd $TmpDir || exit 1
 export Count=1
 echo '\documentclass{article}\usepackage[pdftex]{graphicx}\usepackage{geometry}\geometry{verbose,a4paper,tmargin=10mm,bmargin=10mm,lmargin=10mm,rmargin=10mm,headheight=0mm,headsep=0mm,footskip=0mm}\pagestyle{empty}\begin{document}\raggedright\noindent' > handout.tex
-export IFS=$'\n' # (NB $HOME may have spaces in it on cygwin)
+IFS=$'\n' # (as $HOME may have spaces in it on cygwin)
 for N in $(ls -r -t "$HOME"/.config/GIMP/*/tmp/*-area.png "$HOME"/.gimp*/tmp/*-area.png /tmp/gimp/*/*-area.png "$HOME/Library/Application Support/Gimp/tmp"/*-area.png "$HOME/Library/Application Support/GIMP"/*/tmp/*-area.png /var/folders/*/*/*/gimp/*/*png 2>/dev/null); do
+  unset IFS
   if ! test -e "$N"; then continue; fi # wrong directory
   mv "$N" $Count.png || (cp "$N" $Count.png && rm "$N")
   export Geom=$(pngtopnm $Count.png | head -2 | tail -1)
@@ -48,7 +49,8 @@ for N in $(ls -r -t "$HOME"/.config/GIMP/*/tmp/*-area.png "$HOME"/.gimp*/tmp/*-a
     # Width is greater than height - better put it landscape
     RotStart='\rotatebox{90}{'
     RotEnd='}'
-  else unset RotStart RotEnd; fi
+  else
+      unset RotStart RotEnd; fi
   Aspect=$[1000*$(echo $Geom|sed -e 's/ /\//g')]
   if test $Aspect -gt 1414 || test $Aspect -lt 707; then
     # better scale by the longest side
@@ -75,6 +77,7 @@ if [ "$areas2pdf_force_problem" ] || [ ! -e handout.pdf ]; then
 fi
 mv handout.pdf "$HOME"
 cd
-rm -rf $TmpDir
+#rm -rf $TmpDir
+echo "$TmpDir"
 echo
 echo "Output was written to ~/handout.pdf"
